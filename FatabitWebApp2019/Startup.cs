e9f4e3ec50282fda2d34c29cf50f4a5dfef0d532
takuya.tanaka.813@ctc-g.co.jp <takuya.tanaka.813@ctc-g.co.jp>
2018-12-28 10:00:58 +0900

    プロジェクト ファイルを追加します。

---
+++ FatabitWebApp2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FatabitWebApp2019.Startup))]
+namespace FatabitWebApp2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
